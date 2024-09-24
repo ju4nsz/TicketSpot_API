26a660c1ad8949556e50a94e0dc553fd34b7cf49
Juan Camilo <wxxjuanwxx@hotmail.com>
2024-09-23 20:27:41 -0500

    añadiendo logs de depuración

--- src/main/java/com/wp/reservas/domain/service/impl/usuarios/UsuarioServiceImpl.java
+++ src/main/java/com/wp/reservas/domain/service/impl/usuarios/UsuarioServiceImpl.java
@@ -34,12 +34,17 @@ public class UsuarioServiceImpl implements UsuarioInService {
             throw new HttpGenericException(HttpStatus.BAD_REQUEST, "¡Lo sentimos! Ya existe alguien con ese correo electrónico o usuario, podrías intentar con otro :)");
         }
 
+        log.info("Validando si el género recibio existe, género recibido: {}", usuarioDto.getIdGenero());
         if(!generoPersonasOutService.existeGeneroPersona(usuarioDto.getIdGenero())){
+            log.error("Género no encontrado... {}", usuarioDto.getIdGenero());
             throw new HttpGenericException(HttpStatus.NOT_FOUND, "¡Lo sentimos! No encontramos el género seleccionado en nuestros datos :/");
         }
 
+        log.info("Obteniendo el parámetro de edad mínima para el registro del usuario...");
         Integer edadMinima = Integer.parseInt(parametroOutService.obtenerParametro(DatosGenerales.CLAVE_P_EDAD_MINIMA).getValor());
+        log.info("Parámetro de edad mínima: {}", edadMinima);
 
+        log.info("Validando la edad del usuario contra la edad mínima requerida...");
         if(!usuarioUtil.validarFechaNacimiento(usuarioDto.getFechaNacimiento(), edadMinima)){
             throw new HttpGenericException(HttpStatus.NOT_ACCEPTABLE,
                     "¡Lo sentimos! Para poder ingresar a nuestra app, debes ser mayor de " + edadMinima + " años :/");
