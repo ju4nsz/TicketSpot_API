64912f6e3291c301c87dd73a3d5f3236711f6dc9
herrECoDe <juanandresh1024@gmail.com>
2024-10-18 10:19:48 -0500

    Agregando autorizacion al endpoint

--- src/main/java/com/wp/reservas/web/controller/usuarios/UsuarioFotoController.java
+++ src/main/java/com/wp/reservas/web/controller/usuarios/UsuarioFotoController.java
@@ -3,9 +3,9 @@ package com.wp.reservas.web.controller.usuarios;
 import com.wp.reservas.domain.models.request.usuarios.UsuarioFotoRequest;
 import com.wp.reservas.domain.models.response.GenericResponse;
 import com.wp.reservas.domain.service.in.usuarios.UsuarioFotoInService;
-import com.wp.reservas.domain.service.in.usuarios.UsuarioInService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +18,7 @@ public class UsuarioFotoController {
 
     private final UsuarioFotoInService usuarioFotoInService;
 
+    @PreAuthorize("hasAuthority('usuario:subir-foto')")
     @PostMapping
     public ResponseEntity<GenericResponse> subirFotoUsuario(@ModelAttribute  UsuarioFotoRequest request){
         return ResponseEntity.ok(usuarioFotoInService.subir(request));
